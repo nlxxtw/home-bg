8ffee6419efc93c706334a8191a83ffaba409065
Tang Wu <49474003+nlxxtw@users.noreply.github.com>
2024-09-21 20:06:50 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,10 +15,8 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          <!-- &amp;&nbsp;&nbsp;
-          <a :href="config.github" target="_blank">   -->
-           
-          </a>
+          
+         
         </span>
         <!-- 站点备案 -->
         <span>
