3e138599ea25c5b0f3be8417561bfa0bbac124ba
Tang Wu <49474003+nlxxtw@users.noreply.github.com>
2024-09-21 09:08:02 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="more-content">  
-    相遇是一种缘分！关注微信公众号：<a href="https://image.vredu.icu/img/微信公众号.jpg" target="_blank">zdDown</a> 资源官网：<a href="http://www.zdDown.icu" target="_blank">www.zdDown.icu 你我一起进步！</a>  
+    相遇是一种缘分！关注微信公众号：zdDown 资源官网：<a href="http://www.zdDown.icu" target="_blank">www.zdDown.icu </a>  你我一起进步！
 </div>
 </template>
 
