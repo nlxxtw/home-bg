e807a0a53d24b84c66700c2a41f97191ccb8530e
Tang Wu <49474003+nlxxtw@users.noreply.github.com>
2024-09-22 11:03:32 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -3,15 +3,17 @@
     相遇是一种缘分！   
 </div>  
 <div class="more-content">   
-    关注微信公众号：zdDown   
+   <a href="https://im.19492035.xyz/file/1726973988787_wxgzh.jpg" target ="_blank"> 关注微信公众号：zdDown  </a>
 </div>   
 <br>   
 <div style="text-align: center;">  
-    <img src="https://image.vredu.icu/img/wxgzh.jpg" alt="公告图片" style="max-width: 150px; height: auto;">  
+    <img src="https://im.19492035.xyz/file/1726973988787_wxgzh.jpg" alt="公众号图片" style="max-width: 150px; height: auto;">  
+    <img src="https://im.19492035.xyz/file/1726826500883_小程序码.png" alt="小程序图片" style="max-width: 150px; height: auto;">  
+    <img src="https://im.19492035.xyz/file/1726492076858_gh_53873b9daeb7_258 (1).jpg" alt="公告图片" style="max-width: 150px; height: auto;">  
 </div>  
 <br>   
 <div class="more-content">   
-   <a href="http://www.zdDown.icu" target="_blank"> 直达资源官网</a>  
+   <a href="http://www.zdDown.icu" target="_blank"> 点我直达资源官网</a>  
 </div>
 
 
